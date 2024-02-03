c5c3440bdc0af5649b871fb6fa2d1f66c7e81500
Juan <je.ortiz10003@gmail.com>
2024-02-03 17:22:24 -0500

    Add logo-title with navbar

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ function App() {
   return (
     <Router>
       <div className='mainPage'>
-        <Logo/>
         <Navbar />     
         <Article />
       </div>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,7 +3,10 @@ import {Link} from 'react-router-dom'
 export const Navbar = () => {
     return (
         <div className='nav-bar-container'>
-            <nav className='nav-bar'>
+            <div className="title-container">
+                <h1>Symbo</h1><h1>Lïtê</h1>         
+            </div>
+            <nav className='nav-bar'> 
                 <ul className='nav-bar-menu'>
                     <li>
                     <Link to="/#symbols1">Símbolo De Flecha</Link>
@@ -16,13 +19,7 @@ export const Navbar = () => {
                     </li>
                     <li>
                     <Link to="/#symbols4">Símbolo De Flecha</Link>
-                    </li>
-                    <li>
-                    <Link to="/#symbols5">Símbolo De Flecha</Link>
-                    </li>
-                    <li>
-                    <Link to="/#symbols6">Símbolo De Flecha</Link>
-                    </li>                
+                    </li>              
                 </ul>
             </nav>
             <nav className='nav-bar'>
@@ -38,13 +35,71 @@ export const Navbar = () => {
                     </li>
                     <li>
                     <Link to="/#symbols4">Símbolo De Flecha</Link>
+                    </li>              
+                </ul>
+            </nav>
+            <nav className='nav-bar'>
+                <ul className='nav-bar-menu'>
+                    <li>
+                    <Link to="/#symbols1">Símbolo De Flecha</Link>
                     </li>
                     <li>
-                    <Link to="/#symbols5">Símbolo De Flecha</Link>
+                    <Link to="/#symbols2">Símbolo De Flecha</Link>
                     </li>
                     <li>
-                    <Link to="/#symbols6">Símbolo De Flecha</Link>
-                    </li>                
+                    <Link to="/#symbols3">Símbolo De Flecha</Link>
+                    </li>
+                    <li>
+                    <Link to="/#symbols4">Símbolo De Flecha</Link>
+                    </li>              
+                </ul>
+            </nav>
+            <nav className='nav-bar'>
+                <ul className='nav-bar-menu'>
+                    <li>
+                    <Link to="/#symbols1">Símbolo De Flecha</Link>
+                    </li>
+                    <li>
+                    <Link to="/#symbols2">Símbolo De Flecha</Link>
+                    </li>
+                    <li>
+                    <Link to="/#symbols3">Símbolo De Flecha</Link>
+                    </li>
+                    <li>
+                    <Link to="/#symbols4">Símbolo De Flecha</Link>
+                    </li>              
+                </ul>
+            </nav>
+            <nav className='nav-bar'>
+                <ul className='nav-bar-menu'>
+                    <li>
+                    <Link to="/#symbols1">Símbolo De Flecha</Link>
+                    </li>
+                    <li>
+                    <Link to="/#symbols2">Símbolo De Flecha</Link>
+                    </li>
+                    <li>
+                    <Link to="/#symbols3">Símbolo De Flecha</Link>
+                    </li>
+                    <li>
+                    <Link to="/#symbols4">Símbolo De Flecha</Link>
+                    </li>              
+                </ul>
+            </nav>
+            <nav className='nav-bar'>
+                <ul className='nav-bar-menu'>
+                    <li>
+                    <Link to="/#symbols1">Símbolo De Flecha</Link>
+                    </li>
+                    <li>
+                    <Link to="/#symbols2">Símbolo De Flecha</Link>
+                    </li>
+                    <li>
+                    <Link to="/#symbols3">Símbolo De Flecha</Link>
+                    </li>
+                    <li>
+                    <Link to="/#symbols4">Símbolo De Flecha</Link>
+                    </li>              
                 </ul>
             </nav>
         </div>
