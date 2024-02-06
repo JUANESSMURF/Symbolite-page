70b3eb94b3d71098d637d061376d00ca1eb98775
Juan <je.ortiz10003@gmail.com>
2024-02-06 15:49:30 -0500

    Add and testing different combinations of colors for the page

--- src/Components/Logo.jsx
+++ src/Components/Logo.jsx
@@ -3,7 +3,7 @@
 export const Logo = () => {
     return (
         <div className="title-container">
-            <h1>Symbo</h1><h1>Lïtê</h1>         
+            <h1>Symbo</h1><h1>Lite</h1>         
         </div>
     )
 }
