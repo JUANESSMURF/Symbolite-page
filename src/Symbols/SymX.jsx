72d691f76293bdb7172c59b736a8b0e8e572cc88
Juan <je.ortiz10003@gmail.com>
2024-02-21 21:51:57 -0500

    Add som animation on copy message and updating x symbols with new functions

--- src/Symbols/SymX.jsx
+++ src/Symbols/SymX.jsx
@@ -1,39 +1,46 @@
-import { copyText } from "../Logic"
+import { SymMessage } from "../Logic";
 
 
 
-export const SymX = () => {
+export const SymX = (showMessage) => {
+
+    const { message, handleButtonClick } = SymMessage(showMessage);
 
     return (
         <div className="buttons-container x-container">
             <div className="buttons-title-container">
                 <h2>Símbolos De X</h2>
             </div>
+            {message && (
+                <div className="message">
+                    {message}
+                </div>
+            )}
             <div className="button-symbols-container">
-                <button className="button-x" onClick={() => copyText("×")}>×</button>
-                <button className="button-x" onClick={() => copyText("☓")}>☓</button>
-                <button className="button-x" onClick={() => copyText("✕")}>✕</button>
-                <button className="button-x" onClick={() => copyText("✖")}>✖</button>
-                <button className="button-x" onClick={() => copyText("❌")}>❌</button>
-                <button className="button-x" onClick={() => copyText("❎")}>❎</button>
-                <button className="button-x" onClick={() => copyText("⨉")}>⨉</button>
-                <button className="button-x" onClick={() => copyText("⨯")}>⨯</button>
-                <button className="button-x" onClick={() => copyText("☐")}>☐</button>
-                <button className="button-x" onClick={() => copyText("☒")}>☒</button>
-                <button className="button-x" onClick={() => copyText("✗")}>✗</button>
-                <button className="button-x" onClick={() => copyText("✘")}>✘</button>
-                <button className="button-x" onClick={() => copyText("Χ")}>Χ</button>
-                <button className="button-x" onClick={() => copyText("χ")}>χ</button>
-                <button className="button-x" onClick={() => copyText("ꭓ")}>ꭓ</button>
-                <button className="button-x" onClick={() => copyText("ᚷ")}>ᚷ</button>
-                <button className="button-x" onClick={() => copyText("⊗")}>⊗</button>
-                <button className="button-x" onClick={() => copyText("⨷")}>⨷</button>
-                <button className="button-x" onClick={() => copyText("ₓ")}>ₓ</button>
-                <button className="button-x" onClick={() => copyText("ˣ")}>ˣ</button>
-                <button className="button-x" onClick={() => copyText("𒉽")}>𒉽</button>
-                <button className="button-x" onClick={() => copyText("⛌")}>⛌</button>
-                <button className="button-x" onClick={() => copyText("⛒")}>⛒</button>
-                <button className="button-x" onClick={() => copyText("⛝")}>⛝</button>
+                <button className="button-x" onClick={() => handleButtonClick("×", "×")}>×</button>
+                <button className="button-x" onClick={() => handleButtonClick("☓", "☓")}>☓</button>
+                <button className="button-x" onClick={() => handleButtonClick("✕", "✕")}>✕</button>
+                <button className="button-x" onClick={() => handleButtonClick("✖", "✖")}>✖</button>
+                <button className="button-x" onClick={() => handleButtonClick("❌", "❌")}>❌</button>
+                <button className="button-x" onClick={() => handleButtonClick("❎", "❎")}>❎</button>
+                <button className="button-x" onClick={() => handleButtonClick("⨉", "⨉")}>⨉</button>
+                <button className="button-x" onClick={() => handleButtonClick("⨯", "⨯")}>⨯</button>
+                <button className="button-x" onClick={() => handleButtonClick("☐", "☐")}>☐</button>
+                <button className="button-x" onClick={() => handleButtonClick("☒", "☒")}>☒</button>
+                <button className="button-x" onClick={() => handleButtonClick("✗", "✗")}>✗</button>
+                <button className="button-x" onClick={() => handleButtonClick("✘", "✘")}>✘</button>
+                <button className="button-x" onClick={() => handleButtonClick("Χ", "Χ")}>Χ</button>
+                <button className="button-x" onClick={() => handleButtonClick("χ", "χ")}>χ</button>
+                <button className="button-x" onClick={() => handleButtonClick("ꭓ", "ꭓ")}>ꭓ</button>
+                <button className="button-x" onClick={() => handleButtonClick("ᚷ", "ᚷ")}>ᚷ</button>
+                <button className="button-x" onClick={() => handleButtonClick("⊗", "⊗")}>⊗</button>
+                <button className="button-x" onClick={() => handleButtonClick("⨷", "⨷")}>⨷</button>
+                <button className="button-x" onClick={() => handleButtonClick("ₓ", "ₓ")}>ₓ</button>
+                <button className="button-x" onClick={() => handleButtonClick("ˣ", "ˣ")}>ˣ</button>
+                <button className="button-x" onClick={() => handleButtonClick("𒉽", "𒉽")}>𒉽</button>
+                <button className="button-x" onClick={() => handleButtonClick("⛌", "⛌")}>⛌</button>
+                <button className="button-x" onClick={() => handleButtonClick("⛒", "⛒")}>⛒</button>
+                <button className="button-x" onClick={() => handleButtonClick("⛝", "⛝")}>⛝</button>
             </div>
         </div>
     )
