b395c446554299a7dd11c57414fd680a9ccd0ed7
Juan <je.ortiz10003@gmail.com>
2024-02-21 21:11:59 -0500

    Make changes on chess symbols with new function handlebuttonclick

--- src/Symbols/SymChess.jsx
+++ src/Symbols/SymChess.jsx
@@ -1,27 +1,34 @@
-import { copyText } from "../Logic"
+import { SymMessage } from "../Logic";
 
 
 
-export const SymChess = () => {
+export const SymChess = (showMessage) => {
+
+    const { message, handleButtonClick } = SymMessage(showMessage);
 
     return (
         <div className="buttons-container chess-container">
             <div className="buttons-title-container">
                 <h2>Símbolos De Ajedrez</h2>
             </div>
+            {message && (
+                <div className="message">
+                    {message}
+                </div>
+            )}
             <div className="button-symbols-container">
-                <button className="button-chess" onClick={() => copyText("♔")}>♔</button>
-                <button className="button-chess" onClick={() => copyText("♚")}>♚</button>
-                <button className="button-chess" onClick={() => copyText("♕")}>♕</button>
-                <button className="button-chess" onClick={() => copyText("♛")}>♛</button>
-                <button className="button-chess" onClick={() => copyText("♖")}>♖</button>
-                <button className="button-chess" onClick={() => copyText("♜")}>♜</button>
-                <button className="button-chess" onClick={() => copyText("♘")}>♘</button>
-                <button className="button-chess" onClick={() => copyText("♞")}>♞</button>
-                <button className="button-chess" onClick={() => copyText("♗")}>♗</button>
-                <button className="button-chess" onClick={() => copyText("♝")}>♝</button>
-                <button className="button-chess" onClick={() => copyText("♙")}>♙</button>
-                <button className="button-chess" onClick={() => copyText("♟")}>♟</button>
+                <button className="button-chess" onClick={() => handleButtonClick("♔", "♔")}>♔</button>
+                <button className="button-chess" onClick={() => handleButtonClick("♚", "♚")}>♚</button>
+                <button className="button-chess" onClick={() => handleButtonClick("♕", "♕")}>♕</button>
+                <button className="button-chess" onClick={() => handleButtonClick("♛", "♛")}>♛</button>
+                <button className="button-chess" onClick={() => handleButtonClick("♖", "♖")}>♖</button>
+                <button className="button-chess" onClick={() => handleButtonClick("♜", "♜")}>♜</button>
+                <button className="button-chess" onClick={() => handleButtonClick("♘", "♘")}>♘</button>
+                <button className="button-chess" onClick={() => handleButtonClick("♞", "♞")}>♞</button>
+                <button className="button-chess" onClick={() => handleButtonClick("♗", "♗")}>♗</button>
+                <button className="button-chess" onClick={() => handleButtonClick("♝", "♝")}>♝</button>
+                <button className="button-chess" onClick={() => handleButtonClick("♙", "♙")}>♙</button>
+                <button className="button-chess" onClick={() => handleButtonClick("♟", "♟")}>♟</button>
 
             </div>
         </div>
